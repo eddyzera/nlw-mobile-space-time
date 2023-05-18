777147ba505c0d6ca94891253daa8452cef0b39e
eddyzera <esilva644@gmail.com>
2023-05-18 10:51:16 -0300

    feat: created blur effect

--- App.tsx
+++ App.tsx
@@ -5,18 +5,27 @@ import {
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
 import { BaiJamjuree_600SemiBold } from '@expo-google-fonts/bai-jamjuree'
-import { Text, View } from 'react-native'
+import { Text, ImageBackground } from 'react-native'
+import blurBg from './src/assets/bg-blur.png'
 
 export default function App() {
-  const [] = useFonts({
+  const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_600SemiBold,
   })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
-    <View className="flex-1 items-center justify-center bg-gray-950">
-      <Text className="text-4xl font-bold text-zinc-50">Hello World</Text>
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center bg-gray-950"
+      imageStyle={{ position: 'absolute', left: '-100%' }}
+    >
       <StatusBar style="light" />
-    </View>
+    </ImageBackground>
   )
 }
