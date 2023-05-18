98756f3d238e7d5a25759742141e66d1d332827b
eddyzera <esilva644@gmail.com>
2023-05-18 10:11:22 -0300

    chore: install and configure eslint

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import { BaiJamjuree_600SemiBold } from '@expo-google-fonts/bai-jamjuree'
+import { Text, View } from 'react-native'
 
 export default function App() {
+  const [] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_600SemiBold,
+  })
   return (
-    <View className="bg-gray-950 flex-1 items-center justify-center">
-      <Text className="text-zinc-50 font-bold text-4xl">Hello World</Text>
+    <View className="flex-1 items-center justify-center bg-gray-950">
+      <Text className="text-4xl font-bold text-zinc-50">Hello World</Text>
       <StatusBar style="light" />
     </View>
-  );
+  )
 }
-
